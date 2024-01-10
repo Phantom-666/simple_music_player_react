cf27327b9f9074ed5c83274b06cc8512b93359a9
Vadim <v@gmail.com>
2024-01-10 08:33:53 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react"
+import music from "./sounds/test.mp3"
 
 function App() {
+  const [audio] = useState(new Audio(music))
+
+  const [isPlaying, setIsPlaying] = useState(false)
+  const [seconds, setSeconds] = useState(0)
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (isPlaying) {
+        setSeconds(audio.currentTime)
+      }
+    }, 1000)
+
+    return () => clearInterval(interval)
+  }, [audio, audio.currentTime, isPlaying])
+
+  useEffect(() => {
+    audio.addEventListener("ended", () => setIsPlaying(false))
+    return () => {
+      audio.removeEventListener("ended", () => setIsPlaying(false))
+    }
+  }, [])
+
+  const playFunc = () => {
+    if (isPlaying) {
+      setIsPlaying(false)
+      audio.pause()
+    } else {
+      setIsPlaying(true)
+      audio.play()
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="center">
+          <div className="row">
+            <h2>Simple Music Player</h2>
+
+            <img
+              height={300}
+              src="https://www.rewizor.ru/files/228949ojmf.jpg"
+              alt=""
+            />
+            <br />
+            <div className="row">
+              <div className="col s4"></div>
+              <input
+                type="range"
+                min="0"
+                max={audio.duration ? audio.duration : 100}
+                value={seconds}
+                className="col s4"
+                onChange={(e) => {
+                  audio.currentTime = Number(e.target.value)
+                }}
+              />
+            </div>
+            <div className="row player-menu">
+              <button className="btn" onClick={playFunc}>
+                {isPlaying ? "Stop" : "Play"}
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
